48a3471b3e949639a62a55c4b905bd0f8879037c
Meraxesfire <isabelcaromorillo@gmail.com>
2025-12-24 17:43:16 +0100

    añado funcion de agregar tarea al boton

---
+++ script.js
@@ -0,0 +1,26 @@
+class tarea{
+    constructor(id, desc, registro){
+    this.id= id;
+    this.desc=desc;
+    this.registro=registro;
+    }
+}
+
+const listaTareas = [];
+
+function agregarTarea(descripcion){
+
+    let valorInput =document.getElementById("userInput").value;
+
+if(valorInput==""){
+    alert("ingresa una tarea")
+    return;
+}else{
+    const li=document.createElement("li");
+    li.textContent=valorInput;
+    document.getElementById("list").appendChild(li)
+    document.getElementById("userInput").value = "";
+}
+
+
+}
